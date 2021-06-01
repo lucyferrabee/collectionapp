af1da737af7010e53fae64c6c07c8d04700d37e9
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-06-01 16:30:16 +0100

    updated the index page for unit testing

--- test/index.php
+++ test/index.php
@@ -4,11 +4,19 @@ require '../index.php';
 
 use PHPUnit\Framework\TestCase;
 
-Class index extends TestCase{
-    public function testgetDBfunction() {
-    $result = getDB();
+class index extends TestCase{
+    //public function testgetDBfunction() {
+   // $result = getDB();
 
-    $this->assertIsArray($result);
-    }
+    //$this->assertIsArray();
+   // }
+
+public function testMultiply() {
+      $input = 3;
+      $expected = 6;
+      $result = multiply($input);
+      $this->assertEquals($expected, $result);
+}
+
 }
 ?>
