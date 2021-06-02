2469012632cf5ac63588241b93765430319f2d97
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-06-02 15:57:38 +0100

    hopefully final changes

--- functions.php
+++ functions.php
@@ -1,25 +1,22 @@
 <?php
 
-function getDB(): PDO {
-$db = new PDO('mysql:host=db; dbname=collectionapp', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-return $db;
+function getDB(): PDO
+{
+    $db = new PDO('mysql:host=db; dbname=collectionapp', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db;
 }
 
 function formatRecordsForDisplay(array $record): string
 {
     return 'Artist: ' . $record['Artist'] . '<br>' . 'Title:  ' . $record['Title'] . '<br>Rating:  ' .
-       $record['Rating'] . '<br>Release Date:  ' . $record['Released'] .
-       '<br>Condition:  ' . $record['Condition'] . '<br>' . '<br>';
+        $record['Rating'] . '<br>Release Date:  ' . $record['Released'] .
+        '<br>Condition:  ' . $record['Condition'] . '<br>' . '<br>';
 }
 
 function displayRecords(array $records): string
 {
-foreach ($records as $record) {
-echo formatRecordsForDisplay($record);
+    foreach ($records as $record) {
+        echo formatRecordsForDisplay($record);
+    }
 }
-}
-
-
-
-
